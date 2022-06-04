dffa09d2fa685e95801e008be1b6ea397336b816
Edmund Debrah <edmundking05@gmail.com>
2022-06-03 18:19:14 -0700

    add 5-print_numbers.c and exe

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *main - returns nothing because its void
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
