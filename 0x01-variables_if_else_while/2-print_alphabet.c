e7f06b1cc21b6f1acc26231d700aaf9bc085c32e
Edmund Debrah <edmundking05@gmail.com>
2022-06-03 17:08:31 -0700

    add 2-print_alphabet.c and executable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - starts here
+ * the alphabet in lowercase
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
