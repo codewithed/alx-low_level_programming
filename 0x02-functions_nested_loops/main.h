48c743698ebd9a95583f0ecf7362d90d5fbc13ae
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-07 19:39:43 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+* prints the inputted char
+*/
 int _putchar(char c);
 
+/* prints the alphabet in lowercase with a new line */
+void print_alphabet(void);
 #endif 
   
