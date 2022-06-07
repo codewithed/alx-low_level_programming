b006f586c8e7d6beaa7b57be434de632e92e07fe
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-07 14:23:34 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,33 +1,5 @@
 #include <unistd.h>
 #include <string.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+
 int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-
-/**
- * printword - prints the characters of a string 'word'
- * Return: Void
- */
-/* Prints the word "_putchar"*/
 int printword(void)
-{
-	int i;
-	char c;
-	int len = strlen(word);
-	
-	for (i = 0; i < len; i++)
-	{
-		c = word[i];
-		_putchar(c);
-	}
-	return (0);
-}
