f6a610bdca54ee02bda54b79fc48c74e400fd6a9
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-07 19:37:18 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,9 +5,13 @@
  * Return: Always 0
  */
 
+/* declare function print_alphabet */
+void print_alphabet(void);
+
+
 int main(void)
 {
-	print_alphabet();
+	void print_alphabet(void);
 	return (0);
 }
 
@@ -15,10 +19,9 @@ int main(void)
 void print_alphabet(void)
 {
 	char i;
-	
+
 	for (i = 'a'; i < 'z'; i++)
 	{
 		_putchar(i);
 	}
-	return;
 }
