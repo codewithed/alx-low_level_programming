0c8369980d6bd84097decc982eb9988f964bce5d
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-07 20:04:08 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,11 @@
 #include "main.h"
 
-/**
- * main - check the code
- * Return: Always 0
- */
-
-/* declare function print_alphabet */
-void print_alphabet(void);
-/* prints inputted chars */
-int _putchar(char c);
-
-
-int main(void)
-{
-	void print_alphabet(void);
-	return (0);
-}
-
 
 /**
  * print_alphabet - prints alphabet in lowercase
+ *
  * Returns: void
+ *
  */
 
 void print_alphabet(void)
