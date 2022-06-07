bbac63a531ecf46cdf8dfcfe9acc94faacd2d3dd
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-07 16:28:45 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Start of program
  * Return: Always 0 (Success)
  */
 
-= int main(void)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
