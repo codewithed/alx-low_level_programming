3fb93c7a1167ee54719005c68cb8c3c36511b9e2
codewithed <80069705+codewithed@users.noreply.github.com>
2022-06-08 15:35:06 +0000

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,23 +2,81 @@
 #define MAIN_H
 
 /**
-* prints the inputted char
-*/
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
 int _putchar(char c);
-
-/* prints the alphabet in lowercase with a new line */
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
 void print_alphabet(void);
-
-/* prints the alphabet 10 times*/
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ */
 void print_alphabet_x10(void);
-
-/* checks if a char is lowercase */
+/**
+ * _islower - prints if c is lower case
+ * @c: character to use
+ * Return: 1 or 0
+ */
 int _islower(int c);
-
-/* returns 1 if c is either uppercase or lowercase */
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
 int _isalpha(int c);
-
-/* prints the sign of a number */
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0  or -1
+ */
 int print_sign(int n);
+/**
+ * _abs - gives absolute value
+ * @int: integer
+ * Return: integer or abs value of int
+ */
+int _abs(int);
+/**
+ * print_last_last - prints digit last
+ *
+ * @int: integer to input
+ * Return: last digit of integer
+ */
+int print_last_digit(int);
+/**
+ * jack_bauer - jack bauer gooo
+ *
+ */
+void jack_bauer(void);
+/**
+ * times_table - times this up
+ *
+ *
+ */
+void times_table(void);
+/**
+ * add - adds stuff up
+ *
+ * @int: integers to add
+ * Return: returns a sum
+ */
+int add(int, int);
+/**
+ * print_to_98 - prints to 98
+ * @n: integer to start at
+ *
+ */
+void print_to_98(int n);
+/**
+ * print_times_table - whatever man
+ *@n: integer to put in this table
+ *
+ */
+void print_times_table(int n);
 #endif 
   
